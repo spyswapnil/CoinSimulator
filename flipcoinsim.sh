6b3e5f8ba14e2654d8beac69fac447e09df22ba2
Swapnil Sharma <sharma.swapnil245@gmail.com>
2020-09-02 17:12:33 +0530

    difference between winning added

--- flipcoinsim.sh
+++ flipcoinsim.sh
@@ -1,10 +1,26 @@
 #! /bin/bash
 
-coin=$(( RANDOM%2 ))
-
-if [ $coin -eq 1 ]
+counthead=0
+counttail=0
+while [ $counthead -lt 21 ] && [ $counttail -lt 21 ]
+do
+	coin=$(( RANDOM%2 ))
+        if [ $coin -eq 1 ]
+        then
+                ((counthead++))
+        else
+                ((counttail++))
+        fi
+done
+echo "Head count:$counthead"
+echo "Tail count:$counttail"
+if [ $counthead -ge $counttail ]
 then
-	echo Head
+	diff=$(( counthead-counttail ))
+	echo "Head wins by $diff"
 else
-	echo Tail
+	diff=$(( counttail-counthead ))
+	echo "Tail wins by $diff"
 fi
+
+
