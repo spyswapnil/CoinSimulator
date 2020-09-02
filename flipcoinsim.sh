cd72274c43a49a1ef46b29a448fc3ba25af37c0f
Swapnil Sharma <sharma.swapnil245@gmail.com>
2020-09-02 17:01:04 +0530

    count of head and tails added

--- flipcoinsim.sh
+++ flipcoinsim.sh
@@ -1,10 +1,16 @@
 #! /bin/bash
 
-coin=$(( RANDOM%2 ))
-
-if [ $coin -eq 1 ]
-then
-	echo Head
-else
-	echo Tail
-fi
+counthead=0
+counttail=0
+for (( i=0;i<10;i++ ))
+do
+	coin=$(( RANDOM%2 ))
+	if [ $coin -eq 1 ]
+	then
+		((counthead++))
+	else
+		((counttail++))
+	fi
+done
+echo "Head count:$counthead"
+echo "Tail count:$counttail"
